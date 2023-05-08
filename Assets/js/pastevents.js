667b35e650440a45ddb725beafc8957a2b35f901
Tatiguzman3036 <tatianaguadalupeguzmn@hotmail.com>
2023-05-07 22:46:27 -0300

    task2 JS-cards

---
+++ Assets/js/pastevents.js
@@ -0,0 +1,28 @@
+console.log(data);
+let cards = document.getElementById("cards")
+function presentarCards(events){
+    return `<div class="card fondo-cards " style="width: 18rem;">
+     <img src="${events.image} " class="card-img-top img-card" alt="Festival of the collectivities">
+    <div class="card-body">
+        <h5 class="card-title">${events.name} </h5>
+        <p class="card-text">${events.description} </p>
+        <p class="card-text">${events.category} </p>
+    </div>
+    <div class="d-flex justify-content-evenly col-12">
+        <p>Precio : ${events.price} USD</p>
+        <a href="./Assets/cards/books.html" class="btn btn-primary">See More...</a>
+    </div>
+</div>`
+}
+function crearCard(array, donde) {
+    let template = ""
+    for (let evento of array) {
+        template += presentarCards (evento)
+    }
+    donde.innerHTML = template
+}
+function filtrar(eventos) {
+    return eventos.date < data.currentDate
+}
+let filtroCards = data.events.filter ((filtrar))
+crearCard(filtroCards, cards)
